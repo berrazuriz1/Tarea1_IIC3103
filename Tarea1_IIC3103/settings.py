4c126bc3c6849fec0d42a4d23a0c66e95c5d7263
berrazuriz1 <berrazuriz1@uc.cl>
2020-04-06 17:09:16 -0400

    Heroku configurations

--- Tarea1_IIC3103/settings.py
+++ Tarea1_IIC3103/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Tarea1_IIC3103.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_pyscopg2',
+        'NAME': 'Tarea1Tarea1_IIC3103',
+        'User': 'berrazuriz1',
+        'Password': 'ezvcg059',
+        'HOST': 'localhost',
+        'Port': 5432,
     }
 }
 
@@ -118,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
